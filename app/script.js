4eb0c103d78ea1d24f076d445bef134ee0ad34a3
Nic <nic.dan.costea@gmail.com>
2022-11-03 16:36:29 +0100

    checkpoint - all exercises

--- app/script.js
+++ app/script.js
@@ -5,4 +5,33 @@ console.log("olaaa");
 // Extra 13 - Create a method that counts the number of “travels” on the page
 const allTravels = document.querySelectorAll(".card-image");
 const totalTravels = document.getElementById("totalTravels");
-totalTravels.innerText = `${allTravels.length}`;
+
+// Extra 16 - Delete all cards [why is allCards of length 16?]
+const allCards = document.querySelectorAll(".card");
+const deleteBtn = document.getElementById("deleteBtn");
+const removeAllCards = function () {
+  for (let card of allCards) {
+    card.remove();
+  }
+};
+deleteBtn.addEventListener("click", removeAllCards);
+
+const seeTravels = function () {
+  totalTravels.innerText = `${allCards.length}`;
+};
+seeTravels();
+
+// Etra 17 - hot badge to summer items
+const summerElements = document.querySelectorAll("#summer .card");
+
+const hot = function () {
+  for (let sum of summerElements) {
+    const hotBadge = document.createElement("span");
+    hotBadge.classList.add("badge");
+    hotBadge.classList.add("badge-warning");
+    hotBadge.classList.add("badgeStyling");
+    hotBadge.innerText = "Hot";
+    sum.appendChild(hotBadge);
+  }
+};
+hot();
